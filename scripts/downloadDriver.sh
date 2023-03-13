160a5818a1b35cba8d7fce04b3b91e67e004afb2
Craig <craig.mccarthy@live.ie>
2023-03-14 00:03:14 +1100

    test sh script

--- scripts/downloadDriver.sh
+++ scripts/downloadDriver.sh
@@ -1,50 +1,12 @@
 #!/bin/bash
 
-set -x
+sudo apt-get install xvfb
 
-ROOT_DIR=$(pwd)
-echo "$ROOT_DIR"
-cd "$ROOT_DIR"
+sudo apt-get install unzip
 
-TEST_RESOURCES=common/src/main/resources
-FILE_EXTENSION=""
+CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`
 
-case "$OSTYPE" in
-solaris*) echo "$OSTYPE not supported" ;;
-darwin*) OS='mac64' ;;
-linux*) OS='linux64' ;;
-bsd*) echo "$OSTYPE not supported" ;;
-msys*) OS='win32' FILE_EXTENSION=".exe" ;;
-*) echo "unknown $OSTYPE" ;;
-esac
-
-if [ ! -f "$TEST_RESOURCES/drivers/chromedriver"$FILE_EXTENSION ]; then
-  cd "$TEST_RESOURCES/drivers" &&
-    mkdir "temp" &&
-    cd "temp" &&
-    curl -L -k --output driver.zip https://www.nuget.org/api/v2/package/Selenium.WebDriver.ChromeDriver/ --ssl-no-revoke &&
-    unzip driver.zip &&
-    cd driver/$OS &&
-    cp "chromedriver$FILE_EXTENSION" "$TEST_RESOURCES/drivers" &&
-    chmod +700 "$TEST_RESOURCES/drivers/chromedriver"$FILE_EXTENSION &&
-    cd ../../../ &&
-    rm -rf temp &&
-    cd $ROOT_DIR
-fi
-
-if [ ! -f "$TEST_RESOURCES/drivers/geckodriver"$FILE_EXTENSION ]; then
-  cd "$TEST_RESOURCES/drivers" &&
-    mkdir "temp" &&
-    cd "temp" &&
-    curl -L -k --output driver.zip https://www.nuget.org/api/v2/package/Selenium.WebDriver.GeckoDriver/ --ssl-no-revoke &&
-    unzip driver.zip &&
-    cd driver/$OS &&
-    cp "geckodriver$FILE_EXTENSION" "$TEST_RESOURCES/drivers" &&
-    chmod +700 "$TEST_RESOURCES/drivers/geckodriver"$FILE_EXTENSION &&
-    cd ../../../ &&
-    rm -rf temp &&
-    cd $ROOT_DIR
-fi
-
-#uncomment to keep bash open
-#! /bin/bash
+wget -N http://chromedriver.storage.googleapis.com/${CHROME_DRIVER_VERSION}/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+chmod +x chromedriver
+sudo mv -f chromedriver /home/runner/work/spring-cucumber-testng-parallel-test-harness/spring-cucumber-testng-parallel-test-harness/common/src/main/resources/drivers
