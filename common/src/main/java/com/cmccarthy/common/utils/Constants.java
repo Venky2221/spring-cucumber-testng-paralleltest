0b813de247c357e0145c404b58826cff91aae444
Craig <craig.mccarthy@live.ie>
2023-03-13 23:29:20 +1100

    test sh script

--- common/src/main/java/com/cmccarthy/common/utils/Constants.java
+++ common/src/main/java/com/cmccarthy/common/utils/Constants.java
@@ -10,8 +10,8 @@ public class Constants {
 
     public static final long pollingShort = 100;
 
-    public static String DRIVER_DIRECTORY = System.getProperty("user.dir") + "/../common/src/main/resources/drivers";
+    public static String DRIVER_DIRECTORY = System.getProperty("user.dir") + "../common/src/main/resources/drivers";
 
-    public static String COMMON_RESOURCES = System.getProperty("user.dir") + "/../common/src/main/resources";
+    public static String COMMON_RESOURCES = System.getProperty("user.dir") + "../common/src/main/resources";
 
 }
